70760219da4746c432e6a07b7d3fd89dd88c344f
rakibulhassan07 <rakibalhassan523@gmail.com>
2025-04-23 05:12:18 +0600

    Homne page design

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,22 +6,22 @@ const Home = () => {
                <div className="carousel w-full">
   <div id="item1" className="carousel-item w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+      src="https://i.ibb.co.com/8ZNctYR/pexels-changerstudio-140831.jpg"
       className="w-full" />
   </div>
   <div id="item2" className="carousel-item w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+      src="https://i.ibb.co.com/8ZNctYR/pexels-changerstudio-140831.jpg"
       className="w-full" />
   </div>
-  <div id="item3" className="carousel-item w-full">
+  <div id="item3" className=" w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-      className="w-full" />
+      src="https://i.ibb.co.com/8ZNctYR/pexels-changerstudio-140831.jpg"
+      className="w-full h-2/3" />
   </div>
   <div id="item4" className="carousel-item w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+      src="https://i.ibb.co.com/8ZNctYR/pexels-changerstudio-140831.jpg"
       className="w-full" />
   </div>
 </div>
