439fb090f27067b406032927dfefd6716175ed6b
rakibulhassan07 <rakibalhassan523@gmail.com>
2025-05-09 04:18:01 +0600

    added comments

--- src/components/privateRoute/BakerRoute.jsx
+++ src/components/privateRoute/BakerRoute.jsx
@@ -6,7 +6,7 @@ import { AuthContext } from '../../provider/AuthProvider';
 
 const BakerRoute = ({children}) => {
     const { user } = useContext(AuthContext);
-    const [users] = useUsers(); //current users 
+    const [users] = useUsers(); //current users login 
     const userExist = users.find((userEmail) => userEmail.email === user.email); //match email with current users 
     const location = useLocation();
     if(userExist?.role === 'seller'){
